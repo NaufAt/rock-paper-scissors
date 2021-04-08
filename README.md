6b770ba2034c9010df65d9c8d34c4b6fb9b868e4
“NaufAt” <naufelatia1@gmail.com>
2021-04-08 07:00:03 +0100

    updated rock paper scissors. no longer runs in console. now runs in browser, using buttons and event listeners to trigger events. A running score is now calculated and resets when a player wins or loses (at score 5).

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rock-paper-scissors
+
+
+1. Game is played by pressing a button option of either rock, paper or scissors
+
+2. when the button is pressed, an eventListener calls the playRound function, using button.id as player's choice and computerPlay as the second parameter to generate a random move.
+
+3. with each playRound, the outcome of that event is declared within a div (declareOutcome) / a running score is updated (updateScore) and the score is checked to see if a player has won or loss (checkScore)
+
+4. if a player has won or loss, the checkScore function triggers a declaration of win/loss + resetting of both score and outcome box
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// randomly generates rock/paper or scissors
+// runs as second argument in playround (which runs with every button click)
 function computerPlay() {
     let randInt = Math.floor(Math.random()* 3) + 1;
     if (randInt===1){
@@ -16,81 +18,156 @@ function computerPlay() {
     }
 }
 
+// runs in every playRound event
+//displays the outcome of the round with a DOM method
 function displayOutcome (outcome){
-    alert(outcome);
+    const p = document.querySelector("#outcome");
+    p.textContent = outcome;
 }
 
-function playRound(playerSelection,computerSelection){
-    let playerMove = playerSelection.toLowerCase();
-    let playerScore = null;
 
+//takes parameters from playRound and updates the running score
+// if no input (e.g. draw) - does nothing
+function updateScore(playerScore,computerScore){
+    if (playerScore === 1){
+        const playerScoreDOM = document.querySelector('#playerscore');
+        let result = Number(playerScoreDOM.innerHTML) + 1;
+        playerScoreDOM.textContent = result;
+        
+    }
+    else if (computerScore === 1){
+        const computerScoreDOM = document.querySelector("#computerscore");
+        let result = Number(computerScoreDOM.innerHTML) + 1;
+        computerScoreDOM.textContent = result;
+    };
+};
+
+
+// plays one round, displays Outcome in DOM// updates score / checks if winner 
+function playRound(playerSelection,computerSelection){
+    let playerMove = playerSelection
+    let playerScore = null;
+    let computerScore = null;
+    // draw
     if (playerMove === computerSelection){
         let outcome = "Draw!";
         displayOutcome(outcome);
-        return playerScore = true;
+        updateScore(playerScore,computerScore)
+        checkScore();
+        
+        
     }
     else if (playerMove ==="rock" && computerSelection ==="scissors"){
         playerScore +=1;
         let outcome= "You win, " + playerMove + " beats " + computerSelection + ".";
         displayOutcome(outcome);
-        return playerScore = true;
+        updateScore(playerScore,computerScore);
+        checkScore();
+        return playerScore;
+        
+        
     }
     else if (playerMove ==="scissors" && computerSelection ==="paper"){
         playerScore +=1;
         let outcome= "You win, " + playerMove + " beats " + computerSelection + ".";
         displayOutcome(outcome);
-        return playerScore = true;
+        updateScore(playerScore,computerScore);
+        checkScore();
+        return playerScore;
+        
     }
 
     else if (playerMove ==="paper" && computerSelection ==="rock"){
         playerScore +=1;
         let outcome= "You win, " + playerMove + " beats " + computerSelection + ".";
         displayOutcome(outcome);
-        return playerScore = true;
+        updateScore(playerScore,computerScore);
+        checkScore();
+        return playerScore;
+        
     }
     else {
+        computerScore +=1;
         let outcome = "You lose! " + computerSelection + " beats " + playerMove;
         displayOutcome(outcome);
-    }
-}
-
-function displayWinner(playerScore, computerScore){
-    if (playerScore === computerScore){
-        alert("Final result: You draw");
-    }
-    if (playerScore > computerScore){
-        alert("Final result: You win");
-    }
-    else {
-        alert("Final result: You win");
+        updateScore(playerScore,computerScore);
+        checkScore();
+        return computerScore;
+        
     }
 }
 
 
+// event listeners that use ID of button in place for player selection
+// inside the playRound and thus trigger a playround event
 
-function game (){
-    let round = 5
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while (round > 0){
-        const playerSelection = prompt("Select move: rock, paper or scissors");
-        const computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-        round = round - 1;
-
-        if (playRound===true){
-            playerScore +=1;
-        }
-        else{
-            computerScore +=1;
-        }
+const rock = document.querySelector("#rock");
+rock.addEventListener('click',()=>{
+    playRound(rock.id,computerPlay());
     
+    
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click',()=>{
+    playRound(paper.id,computerPlay());
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click',()=>{
+    playRound(scissors.id, computerPlay());
+});
 
 
+// Runs with every playround event which in turn runs with every button press
+// CHECKS SCORE, IF PLAYER WINS: RESET SCORE , DECLARE WINNER AND RESET OUTCOME
+// VICE VERSA FOR LOSS
+
+function checkScore(){
+    const playerScore = document.querySelector("#playerscore")
+    const computerScore = document.querySelector("#computerscore")
+
+    let runningPlayer= Number(playerScore.innerHTML);
+    let runningComputer = Number(computerScore.innerHTML);
+
+    if (runningPlayer >= 5){
+        declareWin();
+        resetScore();
+        resetOutcomeBox();
     }
-    displayWinner(playerScore,computerScore);
+
+    else if (runningComputer >=5){
+        declareLoss();
+        resetScore();
+        resetOutcomeBox();
+    }
+
 
 }
 
-console.log(game());
+// declares winner when checkScore finds one
+function declareWin() {
+    alert("Congratulations, you won!");
+
+}
+
+// declares a loser when checkScore finds one
+function declareLoss () {
+    alert("You have lost the battle.");
+}
+
+
+// resets score if a winner/loser is found by checkScore
+function resetScore () {
+    const playerScore = document.querySelector("#playerscore");
+    const computerScore = document.querySelector("#computerscore")
+    playerScore.innerHTML = "0";
+    computerScore.innerHTML = "0";
+}
+
+
+function resetOutcomeBox() {
+    const outcome = document.querySelector("#outcome");
+    outcome.textContent = '';
+}
+
